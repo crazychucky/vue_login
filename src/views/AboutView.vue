db56bcc62d333aa1b7f57fcefc74a894bc895887
yuanlixiao <yuanlixiao@live.cn>
2022-12-11 21:08:42 +0800

    chore:test codes

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,5 +1,27 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
-  </div>
+  <span>Message: {{"base"
+            ? `var(--el-border-radius-"base")`
+            : ''}}</span>
+   <div
+        class="radius1"
+        :style="{
+          borderRadius: 'var(--el-border-radius-round)',
+        }"
+      />
 </template>
+
+<script lang="ts" setup>
+import { ref } from 'vue'
+
+</script>
+<style scoped>
+.radius1 {
+  height: 400px;
+  width: 50%;
+  border: 2px solid var(--el-border-color);
+  margin-top: 20px;
+  /* box-shadow: var(--el-box-shadow-dark), */
+  box-shadow: 0 2px 4px rgba(0,0,0,0.12),0 0 6px rgba(0,0,0,0.04)
+  /* boxShadow: 'var(--el-box-shadow-dark)', */
+}
+</style>
