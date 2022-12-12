40f6e20a6b6ca8cb19a077b1d810fc2644310420
yuanlixiao <yuanlixiao@live.cn>
2022-12-12 22:43:10 +0800

    chore:update css

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -4,10 +4,7 @@
             : ''}}</span>
    <div
         class="radius1"
-        :style="{
-          borderRadius: 'var(--el-border-radius-round)',
-        }"
-      />
+    />
 </template>
 
 <script lang="ts" setup>
@@ -17,9 +14,12 @@ import { ref } from 'vue'
 <style scoped>
 .radius1 {
   height: 400px;
-  width: 50%;
-  border: 2px solid var(--el-border-color);
+  width: 800px;
+  border: 1px solid var(--el-border-color);
+  border-radius: 50px;
   margin-top: 20px;
+  background-color:aqua;
+  fill:rgb(8, 8, 8);
   /* box-shadow: var(--el-box-shadow-dark), */
   box-shadow: 0 2px 4px rgba(0,0,0,0.12),0 0 6px rgba(0,0,0,0.04)
   /* boxShadow: 'var(--el-box-shadow-dark)', */
