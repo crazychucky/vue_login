1a81a339c5a178a08dad6e87d5513c9803165331
yuanlixiao <yuanlixiao@live.cn>
2022-12-15 21:22:21 +0800

    feat:bg color

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -2,9 +2,9 @@
   <span>Message: {{"base"
             ? `var(--el-border-radius-"base")`
             : ''}}</span>
-   <div
-        class="radius1"
-    />
+   <div class="radius1">
+    <div>OK</div>     <!--TODO:嵌套登陆 -->
+   </div>
 </template>
 
 <script lang="ts" setup>
@@ -13,11 +13,13 @@ import { ref } from 'vue'
 </script>
 <style scoped>
 .radius1 {
-  height: 400px;
-  width: 800px;
+  height: 600px;
+  width: 1200px;
   border: 1px solid var(--el-border-color);
   border-radius: 50px;
-  background-color:aqua;
+  /* background-color:aqua; */
+  
+  background: linear-gradient(to right, #0229bf 0%,#0229bf 50%,white 50%,white 100%);
   fill:rgb(8, 8, 8);
   /* box-shadow: var(--el-box-shadow-dark), */
   box-shadow: 0 2px 4px rgba(0,0,0,0.12),0 0 6px rgba(0,0,0,0.04);
