a9232bf1b779f432b26028d53c374322ddaae788
yuanlixiao <yuanlixiao@live.cn>
2022-12-15 18:59:34 +0800

    chore:center

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -17,11 +17,16 @@ import { ref } from 'vue'
   width: 800px;
   border: 1px solid var(--el-border-color);
   border-radius: 50px;
-  margin-top: 20px;
   background-color:aqua;
   fill:rgb(8, 8, 8);
   /* box-shadow: var(--el-box-shadow-dark), */
-  box-shadow: 0 2px 4px rgba(0,0,0,0.12),0 0 6px rgba(0,0,0,0.04)
+  box-shadow: 0 2px 4px rgba(0,0,0,0.12),0 0 6px rgba(0,0,0,0.04);
+  position: absolute;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  margin: auto;
   /* boxShadow: 'var(--el-box-shadow-dark)', */
 }
 </style>
